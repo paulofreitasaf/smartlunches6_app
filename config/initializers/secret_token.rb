8b129f08594be83f557ea973dde52ec800152bb0
paulofreitasaf <paulofreitasaf@gmail.com>
2013-03-10 22:13:30 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartlunches6App::Application.config.secret_token = '88d87f53548f83b9c9e44b67eceb8d58038f3f854184b4c64d40b86818abc5882a7d1d575b14592db245ff8d449245cde2aa71fa20f4150772845cf6f2b14df7'
